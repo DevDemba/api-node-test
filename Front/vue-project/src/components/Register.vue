86e0393a0bd9a3a7550f315ed79329ca7581f6bf
hassan <noreez.hassan@gmail.com>
2019-06-25 16:47:20 +0200

    update register file

--- Front/vue-project/src/components/Register.vue
+++ Front/vue-project/src/components/Register.vue
@@ -6,12 +6,43 @@
 
 <script>
 export default {
-  name: 'Register'
-}
+  name: 'Register',
+data() {
+            return {
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+                        this.$emit("authenticated", true);
+                        this.$router.replace({ name: "secure" });
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
+<style scoped>
+    #login {
+        width: 500px;
+        border: 1px solid #CCCCCC;
+        background-color: #FFFFFF;
+        margin: auto;
+        margin-top: 200px;
+        padding: 20px;
+    }
+
+    #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -20,3 +51,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
