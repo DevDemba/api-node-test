ecb3aaa8a4fe3fc513e2b3d50bd297c5fee0f272
DemsDevops <m.demba.dia@gmail.com>
2019-06-25 12:01:17 +0200

    update file sql in project

--- Backend/sql/e-motion.sql
+++ Backend/sql/e-motion.sql
@@ -11,7 +11,16 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Les données exportées n'étaient pas sélectionnées.
+-- Export de données de la table e-motion.users : ~5 rows (environ)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+	(1, 'test', 'test@gmail.com', 'test', '2019-02-28 13:20:20'),
+	(2, 'john', 'john@gmail.com', '123456', '2019-02-28 13:20:20'),
+	(3, 'toto', 'toto@gmail.com', 'toto1', '2019-02-28 13:20:20'),
+	(4, 'admin', 'admin@gmail.com', 'admin', '2019-02-28 13:20:20'),
+	(5, 'mhd', 'mhd@gmail.com', 'mhd2019', '2019-02-28 13:20:20');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # api-node-test
+
+
+comment démarrer le projet
+
+cmd => npm start 
+
+Front
+
+cmd => npm run dev
