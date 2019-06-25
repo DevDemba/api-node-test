46222b022eb25a7b30db4d3b42ac63b6b491f4f6
DemsDevops <m.demba.dia@gmail.com>
2019-06-25 15:38:36 +0200

    update link router register/login

--- Front/vue-project/src/router/index.js
+++ Front/vue-project/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Users from '@/components/Users'
+import Register from '@/components/Register'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
