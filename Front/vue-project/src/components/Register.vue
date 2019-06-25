a7df12e120850c5c8ea7c104272565dccac12f44
hassan <noreez.hassan@gmail.com>
2019-06-25 15:04:01 +0200

    update register file

---
+++ Front/vue-project/src/components/Register.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="register">
+    <h1> Register </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
