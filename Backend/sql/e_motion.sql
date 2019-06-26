9f1f47d3e331cc20e37d80722881af7b5666ea96
hassan <noreez.hassan@gmail.com>
2019-06-26 17:24:04 +0200

    update db

--- Backend/sql/e_motion.sql
+++ Backend/sql/e_motion.sql
@@ -1,43 +1,214 @@
--- --------------------------------------------------------
--- Hôte :                        127.0.0.1
--- Version du serveur:           10.3.12-MariaDB - mariadb.org binary distribution
--- SE du serveur:                Win32
--- HeidiSQL Version:             9.4.0.5125
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 26 Juin 2019 à 15:23
+-- Version du serveur :  5.7.14
+-- Version de PHP :  7.2.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `e_motion`
+--
 
--- Export de la structure de la base pour e_motion
-DROP DATABASE IF EXISTS `e_motion`;
-CREATE DATABASE IF NOT EXISTS `e_motion` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `e_motion`;
+-- --------------------------------------------------------
 
--- Export de la structure de la table e_motion. users
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `password` varchar(200) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+--
+-- Structure de la table `admin`
+--
 
--- Export de données de la table e_motion.users : ~5 rows (environ)
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
-	(1, 'test', 'test@gmail.com', 'test', '2019-02-28 13:20:20'),
-	(2, 'john', 'john@gmail.com', '123456', '2019-02-28 13:20:20'),
-	(3, 'toto', 'toto@gmail.com', 'toto1', '2019-02-28 13:20:20'),
-	(4, 'admin', 'admin@gmail.com', 'admin', '2019-02-28 13:20:20'),
-	(5, 'mhd', 'mhd@gmail.com', 'mhd2019', '2019-02-28 13:20:20');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `car`
+--
+
+CREATE TABLE `car` (
+  `id_car` int(11) NOT NULL,
+  `modele` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `moto`
+--
+
+CREATE TABLE `moto` (
+  `id_moto` int(11) NOT NULL,
+  `modele` varchar(255) NOT NULL,
+  `nb_tire` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `offer`
+--
+
+CREATE TABLE `offer` (
+  `id_offer` int(11) NOT NULL,
+  `id_vehicle` int(11) NOT NULL,
+  `date_online_offer` date NOT NULL,
+  `date_end_offer` date NOT NULL,
+  `date_update_offer` date NOT NULL,
+  `price_offer` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rental`
+--
+
+CREATE TABLE `rental` (
+  `id_rental` int(11) NOT NULL,
+  `id_users` int(11) NOT NULL,
+  `id_offer` int(11) NOT NULL,
+  `start_rental_date` date NOT NULL,
+  `end_rental_date` date NOT NULL,
+  `back_rental_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `birthday` date NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `license_driver` varchar(255) NOT NULL,
+  `point` int(11) NOT NULL,
+  `register_date` date NOT NULL,
+  `gender` varchar(12) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicles`
+--
+
+CREATE TABLE `vehicles` (
+  `id_vehicle` int(11) NOT NULL,
+  `marque` varchar(255) NOT NULL,
+  `serial_number` varchar(255) NOT NULL,
+  `color` varchar(64) NOT NULL,
+  `nb_plate` varchar(255) NOT NULL,
+  `nb_kilometer` int(11) NOT NULL,
+  `purchase_date` date NOT NULL,
+  `price` int(11) NOT NULL,
+  `available` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Index pour la table `car`
+--
+ALTER TABLE `car`
+  ADD PRIMARY KEY (`id_car`);
+
+--
+-- Index pour la table `moto`
+--
+ALTER TABLE `moto`
+  ADD PRIMARY KEY (`id_moto`);
+
+--
+-- Index pour la table `offer`
+--
+ALTER TABLE `offer`
+  ADD PRIMARY KEY (`id_offer`);
+
+--
+-- Index pour la table `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  ADD PRIMARY KEY (`id_vehicle`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `car`
+--
+ALTER TABLE `car`
+  MODIFY `id_car` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `moto`
+--
+ALTER TABLE `moto`
+  MODIFY `id_moto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `offer`
+--
+ALTER TABLE `offer`
+  MODIFY `id_offer` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `vehicles`
+--
+ALTER TABLE `vehicles`
+  MODIFY `id_vehicle` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
