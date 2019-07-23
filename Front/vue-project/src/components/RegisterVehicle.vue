5e1332c7a1048330d35631275bfbca80f947fc08
hassan <noreez.hassan@gmail.com>
2019-07-23 14:38:50 +0200

    Update file

--- Front/vue-project/src/components/RegisterVehicle.vue
+++ Front/vue-project/src/components/RegisterVehicle.vue
@@ -1,75 +1,19 @@
-<!--
-<template>
-  <div class="register">
-      <h1>Register</h1>
-        <form>
-            <input type="radio" name="gender" value="Male" checked/> Male
-            <input type="radio" name="gender" value="Female"/> Female
-            <b-form-input type="text" name="lastname" placeholder="Lastname" />    
-            <b-form-input type="text" name="firstname" placeholder="Firstname" />    
-            <b-form-input type="date" name="birthday" placeholder="Birthday" />    
-            <b-form-input type="text" name="address" placeholder="Address" />    
-            <b-form-input type="text" name="phone" placeholder="Phone" />    
-            <b-form-input type="text" name="license_driver" placeholder="License driver " />    
-            <b-form-input type="text" name="email" placeholder="Email" />    
-            <b-form-input type="password" name="password" v-model="password" placeholder="Password" required />  
-            <b-form-input type="password" name="password_confirmation" v_model="password_confirmation" placeholder="Password confirmation" required />  
-            <div>
-                <select v-model="is_admin">
-                    <option value=1>Yes</option>
-                    <option value=0>No</option>
-                </select>
-            </div> 
-            <b-button type="submit" @click="handleSubmit">Register</b-button>  
-        </form>  
-        </div>
-</template> -->
 
 <template>
     <div>
         <h4>Register Vehicle</h4>
         <form>
 
-            <label for="marque" >Marque </label>
-            <div>
-                <input id="marque" type="text" v-model="marque" required>
-            </div>
+            <b-form-input type="text" v-model="marque" placeholder="Marque" required />
+            <b-form-input type="text" v-model="serial_number" placeholder="Serial Number" required />
+            <b-form-input type="color" v-model="color" placeholder="Color" required />
+            <b-form-input type="text" v-model="nb_plate" placeholder="Number Plate" required />
+            <b-form-input type="text" v-model="nb_kilometer" placeholder="Number of kilometer" required/>
+            <b-form-input type="date" v-model="purchase_date" placeholder="Purchase date " required />
+            <b-form-input type="price" v-model="price" placeholder="Price" required />
+           
+            <b-button type="submit" @click="handleSubmit">Validate !</b-button>
 
-            <label for="serial_number" > Serial Number </label>
-            <div>
-                <input id="serial_number" type="text" v-model="serial_number" required>
-            </div>
-
-            <label for="color" >color</label>
-            <div>
-                <input id="color" type="color" v-model="color" required>
-            </div>
-
-            <label for="nb_plate" >Number plate</label>
-            <div>
-                <input id="nb_plate" type="text" v-model="nb_plate" required>
-            </div>
-
-            <label for="nb_kilometer" >Number of kilometer</label>
-            <div>
-                <input id="nb_kilometer" type="text" v-model="nb_kilometer" required>
-            </div>
-
-            <label for="purchase_date" >Purchase date</label>
-            <div>
-                <input id="purchase_date" type="date" v-model="purchase_date" required>
-            </div>
-            
-            <label for="price" >Price</label>
-            <div>
-                <input id="price" type="text" v-model="price" required>
-            </div>
-
-            <div>
-                <button type="submit" @click="handleSubmit">
-                    Register
-                </button>
-            </div>
         </form>
     </div>
 </template>
@@ -116,18 +60,13 @@ export default {
                         localStorage.setItem('jwt',response.data.token)
                         console.log(response.data.vehicle)
                         if (localStorage.getItem('jwt') != null){
-                            this.$emit('loggedIn')
-                            if(this.$route.params.nextUrl != null){
-                                this.$router.push(this.$route.params.nextUrl)
-                            }
-                            else{
-                                this.$router.push('/')
-                            }
+                                alert("Add car")
+                                this.$router.push('/offer')
                         }
                     })
                     .catch(error => {
                         console.error(error);
-                    });
+                    });              
             }
         }
     }
