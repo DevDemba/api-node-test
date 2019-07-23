c6ac9c2c09e11388141e28e489c47841248f0c21
DemsDevops <m.demba.dia@gmail.com>
2019-07-23 19:06:44 +0200

    add vehicle methods in API

--- Backend/Router/UserRouter.js
+++ Backend/Router/UserRouter.js
@@ -140,11 +140,19 @@ router.post('/api/register', function(req, res) {
             res.status(200).send({ auth: true, token: token, user: user });
         }); 
     });
+})
+
+// Retrieve all vehicles
+router.get('/api/vehicle', (req, res) => {
+  dbConn.query('SELECT * FROM vehicles', function (error, results, fields) {
+    if (error) throw error;
+    return res.json({ error: false, data: results, message: 'vehicles list.' });
+  });
 });
 
 
 router.post('/api/vehicle', (req, res) => {
-  user =  [
+  vehicle =  [
       req.body.marque,
       req.body.serial_number,
       req.body.color,
@@ -156,12 +164,28 @@ router.post('/api/vehicle', (req, res) => {
      
   ]; console.log(req.body.color); 
   
-  dbConn.query('INSERT INTO vehicles (marque, serial_number, color, nb_plate, nb_kilometer, purchase_date, price) VALUES (?,?,?,?,?,?,?)', user,
+  dbConn.query('INSERT INTO vehicles (marque, serial_number, color, nb_plate, nb_kilometer, purchase_date, price) VALUES (?,?,?,?,?,?,?)', vehicle,
 
   (err, results) => {
       if (err) return res.status(500).send("There was a problem registering the vehicles.")
           res.status(200).send({ auth: true, token: token, user: user });
   }); 
+});
+
+// Retrieve vehicle with id 
+router.get('/api/vehicle/:id', (req, res)=> {
+  
+    let vehicle_id = req.params.id;
+  
+    if (!vehicle_id) {
+        return res.status(400).send({ error: true, message: 'Please provide vehicle_id' });
+    }
+  
+    dbConn.query('SELECT * FROM vehicles where id_vehicle = ?', vehicle_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.json({ error: false, data: results[0], message: 'vehicle list.' });
+    });
+  
 });
 
 
