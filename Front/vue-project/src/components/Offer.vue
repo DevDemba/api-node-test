737e2b2d9e962e9b01ec33f00a135172ca237444
Demba <m.demba.dia@gmail.com>
2019-07-29 17:03:50 +0200

    changed method to offer

--- Front/vue-project/src/components/Offer.vue
+++ Front/vue-project/src/components/Offer.vue
@@ -1,22 +1,24 @@
 <template>
     <div>
         <h1>Offer</h1>
-        <div class="card" v-for="vehicle in vehicles" :key="vehicle.id">
-            <b-card
+        <div class="parent">
+            <div class="card">
+            <b-card  v-for="vehicle in vehicles" :key="vehicle.id"
             :title="`${vehicle.marque}`"
             img-src="https://picsum.photos/600/300/?image=25"
             img-alt="Image"
             img-top
             tag="article"
             style="max-width: 20rem;"
-            class="mb-2">
+            class="mb-2 ">
             <b-card-text>
               
             </b-card-text>
             <router-link :to="{ name:'Offer-detail', params: {id: vehicle.id_vehicle } }"><b-button variant="primary">View-detail</b-button></router-link>
             </b-card>
         </div>
-    </div>
+        </div>
+    </div> 
 </template>
 
 <script>
@@ -26,10 +28,11 @@ export default {
     name: 'Offer',
     data() {
         return {
+            files: [],
             vehicles: [],
         }
     },
-    beforeCreate() {
+    beforeMount() {
       axios.get('/api/vehicle')
         .then(response => {
           this.vehicles = response.data.data;
@@ -39,22 +42,37 @@ export default {
         })
     },
     created() {
-      axios.get('http://localhost:3000/api/vehicle/:id')
+        // let uploadedFiles = this.$refs.file.files[0];
+        // for( var i = 0; i < uploadedFiles.length; i++ ){
+        //   this.files.push( uploadedFiles[i] );
+        // }
+        // console.log(this.files)
+
+      axios.get('/api/vehicle/:id')
         .then(response => {
           this.vehicles = response.data.data;
         })
         .catch(e => {
           this.errors = e;
         })
-    }   
+    }
+       
 }
 </script>
 
 <style scoped>
-.card {
+.parent {
     display: flex;
-    flex-direction: column;
     justify-content: center;
+    flex-wrap:wrap;
+    margin:-10px 0 0 -10px;
+}
+.card {
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  margin-top: 10px;
+  width: calc(100% * (1/4) - 50px - 1px);
 }
 .error {
   color: red;
