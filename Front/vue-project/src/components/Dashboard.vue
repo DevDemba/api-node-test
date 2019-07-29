f0db25e7d4f79a2e69c9381315d7b386ad63c2d6
DemsDevops <m.demba.dia@gmail.com>
2019-07-29 04:56:03 +0200

    get user with localstorage info

--- Front/vue-project/src/components/Dashboard.vue
+++ Front/vue-project/src/components/Dashboard.vue
@@ -1,54 +1,43 @@
 <template>
     <div>
         <h2>Dashboard</h2>
-        <p>Lastame: {{ user.lastname }}</p>
-        <p>Fisrtname: {{ user.firstname }}</p>
-        <p>Email: {{ user.email }}</p>
-        <p>Address: {{ user.address }}</p>
-        <p>Phone: {{ user.phone }}</p>
-        <p>Birthday: {{ user.birthday }}</p>
-        <p>License driver: {{ user.license_driver }}</p>
-        <p>Date of register: {{ user.register_date }}</p>
-        <p><strong>Point: {{ user.point }}</strong></p>
+        <p>Lastname: {{ user[0].lastname }}</p>
+        <p>Fisrtname: {{ user[0].firstname }}</p>
+        <p>Email: {{ user[0].email }}</p>
+        <p>Address: {{ user[0].address }}</p>
+        <p>Phone: {{ user[0].phone }}</p>
+        <p>Birthday: {{ user[0].birthday }}</p>
+        <p>License driver: {{ user[0].license_driver }}</p>
+        <p>Date of register: {{ user[0].register_date }}</p>
+        <p><strong>Point: {{ user[0].point }}</strong></p>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
-import router from '../router';
 
 export default {
 
-  name: 'Login',
+  name: 'Dashboard',
   data () {
     return {
       user: []
     }
   },
-  methods: {
-    getUsers: function () {
-      axios.get('/api/users')
-        .then(response => {
-          this.users = response.data.data;
-          console.log(this.users)
-        })
-        .catch(e => {
-          this.errors = e;
-          router.push('/');
-        })
-    },
-     showUser: function () {
-      axios.get('/api/users/' + this.$route.params.id)
-        .then(response => {
-          this.user = response.data.data;
-        })
-        .catch(e => {
-          this.errors = e;
-        })
-    }
-  },
-  created () {
-    this.showUser()
+  created() {
+      this.user = JSON.parse(localStorage.getItem('user'));
+      console.log(this.user)
+
+    /* axios.get('/api/users/:id')
+    .then(response => {
+      this.user = response.data.data;
+       console.log(response)
+    })
+    .catch(e => {
+      this.errors = e;
+      router.push('/');
+    }) */
+   // this.showProfile()
   }
 }
 </script>
