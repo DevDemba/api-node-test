e16c608197798d998fd9fa84a6c5f2a40ca1e816
Demba <m.demba.dia@gmail.com>
2019-07-30 17:18:10 +0200

    use momentjs for create 2 filters formatDate & birthday

--- Front/vue-project/src/main.js
+++ Front/vue-project/src/main.js
@@ -9,15 +9,30 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vuetify from 'vuetify'
 // index.js or main.js
 import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
+import moment from 'moment'
 
 Vue.use(BootstrapVue, Vuetify)
 
 Vue.config.productionTip = false
+
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('MM/DD/YYYY hh:mm')
+  }
+});
+
+Vue.filter('birthday', function (value, format) {
+  if (value) {
+      return moment(String(value)).format(format || 'MM/DD/YYYY');
+  }
+})
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+  // render: h => h(App),
+}).$mount('#app')
