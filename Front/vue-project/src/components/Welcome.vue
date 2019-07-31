85eae7f2cef2a3981f0672fd922281d2c7b9c08a
Demba <m.demba.dia@gmail.com>
2019-07-31 18:17:16 +0200

    change images & title in homepage

--- Front/vue-project/src/components/Welcome.vue
+++ Front/vue-project/src/components/Welcome.vue
@@ -1,8 +1,9 @@
 <template>
-    <div class="card">
+    <b-container fluid>
+        <div class="card ">
         <div>
-            <b-card-group deck>
-                <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            <b-card-group deck>       
+                <b-card title="Car" img-src="https://i.ibb.co/zFMDQHt/voiture1.jpg" img-alt="Car" img-top>
                 <b-card-text>
                     This is a wider card with supporting text below as a natural lead-in to additional content.
                     This content is a little bit longer.
@@ -10,14 +11,14 @@
                 <div slot="footer"><small class="text-muted">Last updated 3 mins ago</small></div>
                 </b-card>
 
-                <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+                <b-card title="Rent" img-src="https://i.ibb.co/JvZBj2V/shopping1.jpg" img-alt="Rent" img-top>
                 <b-card-text>
                     This card has supporting text below as a natural lead-in to additional content.
                 </b-card-text>
                 <div slot="footer"><small class="text-muted">Last updated 3 mins ago</small></div>
                 </b-card>
 
-                <b-card title="Title" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+                <b-card title="Moto" img-src="https://i.ibb.co/0rtqqF9/moto1.jpg" img-alt="Moto" img-top>
                 <b-card-text>
                     This is a wider card with supporting text below as a natural lead-in to additional content.
                     This card has even longer content than the first to show that equal height action.
@@ -27,4 +28,8 @@
             </b-card-group>
         </div>
     </div>
+    </b-container>
 </template>
+
+// https://picsum.photos/id/111/4400/2656
+// https://picsum.photos/id/551/7102/4740
