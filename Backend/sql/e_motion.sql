0d1b93d201fcb54c90364ac83d8f2e3cd5eb2ef6
Demba <m.demba.dia@gmail.com>
2019-08-02 17:53:23 +0200

    update database

--- Backend/sql/e_motion.sql
+++ Backend/sql/e_motion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  lun. 29 juil. 2019 à 10:35
+-- Généré le :  ven. 02 août 2019 à 17:51
 -- Version du serveur :  10.3.16-MariaDB
 -- Version de PHP :  7.3.6
 
@@ -114,10 +114,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `lastname`, `firstname`, `birthday`, `address`, `phone`, `license_driver`, `point`, `register_date`, `gender`, `email`, `password`, `is_admin`) VALUES
-(7, 'Demba', 'DIA', '2019-07-10', '30 Rue Montéra', 783438693, 'eeeee', 0, '2019-07-22 14:40:29', 'Male', 'm.demba.dia@gmail.com', '$2b$08$5/U7h6lr3APs2eaME3MKqu/1rxwOHkvzA35Tgl2ZRz1KfY55JM7eG', NULL),
+(7, 'Demba', 'DIA', '2019-07-10', '30 rue de la paix', 783438693, 'eeeee', 0, '2019-07-22 14:40:29', 'Male', 'm.demba.dia@gmail.com', '$2b$08$5/U7h6lr3APs2eaME3MKqu/1rxwOHkvzA35Tgl2ZRz1KfY55JM7eG', NULL),
 (8, 'noreez ', 'noreez', '2019-07-10', 'pakistan', 23656256, 'oui', 0, '2019-07-22 14:46:15', 'Female', 'noreez@gmail.fr', '$2b$08$eb8.6seLij.urb45RJf8I.HLi1pM9yX2ix3X6AEBfhHvp4OkrUJra', NULL),
-(9, 'test', 'test', '2019-07-03', '30 rue de paris', 643679456, 'non', 0, '2019-07-22 14:58:49', 'Male', 'test@gmail.com', '$2b$08$8ufb3Cco6ZRgdC/0SJrfFOzPNZ1RMl4PRRxyMdyXMwTBo/sY4QkR2', NULL),
-(10, 'admin', 'admin', '2019-07-17', 'vue', 120436879, 'oui', 0, '2019-07-22 16:45:33', 'Male', 'admin@gmail.com', '$2b$08$5VOeRtzbNfdfAZBgv6puvub0u9Vxd8/WZtNRgat5t6qKkHu2TPmgm', NULL),
+(9, 'test', 'test', '2019-07-03', '15 rue de paris', 643679456, 'non', 0, '2019-07-22 14:58:49', 'Male', 'test@gmail.com', '$2b$08$8ufb3Cco6ZRgdC/0SJrfFOzPNZ1RMl4PRRxyMdyXMwTBo/sY4QkR2', NULL),
+(10, 'admin', 'admin', '2019-07-17', 'vue', 120436879, 'oui', 0, '2019-07-22 16:45:33', 'Male', 'admin@gmail.com', '$2b$08$5VOeRtzbNfdfAZBgv6puvub0u9Vxd8/WZtNRgat5t6qKkHu2TPmgm', 1),
 (11, 'SHAH', 'Noreez', '2019-07-03', 'afafa', 888884048, 'oui', 0, '2019-07-22 17:23:36', 'Male', 'directeur@gmail', '$2b$08$lAmrOEb508yLmV6/uyc4uueyu2QhaGWnO7xq1lmmQPlvPAF2.wrT2', NULL),
 (14, 'jean', 'paul', '2019-07-17', 'nice ', 2147483647, 'non', 0, '2019-07-29 10:17:07', 'Male', 'jp@gmail.coom', '$2b$08$48IFpcUGxnDFZU5w2bfCx.VHJh7/nOAQ7ZShqvSumM1VVL8WYn23C', NULL),
 (15, 'lorem', 'ipsum', '2019-07-10', 'rome', 488566506, 'oui', 0, '2019-07-29 10:19:33', 'Male', 'lorem-ipsum@gmail.com', '$2b$08$gsONupEPg7jDb0pC6ogcfuEnFLaPb0U.M97RNKJX28TiEIQpyd.LS', NULL);
@@ -148,7 +148,24 @@ CREATE TABLE `vehicles` (
 INSERT INTO `vehicles` (`id_vehicle`, `image`, `marque`, `serial_number`, `color`, `nb_plate`, `nb_kilometer`, `purchase_date`, `price`, `available`) VALUES
 (1, '', 'test', '0080080', '', '54008', 8048048, '2019-07-23', 100, 0),
 (2, '', 'test', '0080080', '#c0c0c0', '54008', 8048048, '2019-07-23', 100, 0),
-(3, '', 'test', '1650084', '#008000', '5800480', 80840880, '2019-07-13', 100, 0);
+(3, '', 'test', '1650084', '#008000', '5800480', 80840880, '2019-07-13', 100, 0),
+(4, '', 'audi', '123896699', '#408080', 'eg-564-jk', 30000, '2019-07-17', 6000, 0),
+(5, '', 'test', '18599', '#004040', '5z4e8r', 30000, '2019-07-10', 15000, 0),
+(6, '', 'isogeo', '188595954749', '#008040', '54-fdfg-52', 30000, '2019-07-11', 30000, 0),
+(7, '', 'verte', '2549595', '#00ff00', '25959f66', 30000, '2019-07-16', 1500, 0),
+(8, '', 'verte', '2549595', '#00ff00', '25959f66', 30000, '2019-07-16', 1500, 0),
+(10, '', 'zdreer', 'retrztez', '#408080', 'zettee', 0, '2019-07-10', 15000, 0),
+(12, '', 'ferrari', '368363498', '#ff0000', 'sz546gh', 35000, '2019-07-17', 4500, 0),
+(13, '', 'ferrari', '368363498', '#ff0000', 'sz546gh', 35000, '2019-07-17', 4500, 0),
+(14, '', 'rttty', 'yyy', '#800080', 'tttt', 0, '2019-07-10', 15000, 0),
+(15, '', 'mini', '149849693', '#ffff00', 'fg-549-he', 28000, '2019-07-17', 12580, 0),
+(16, '', 'ford', '5484869569', '#ffffff', 'zz-476-aa', 60540, '2019-08-05', 22500, 0),
+(17, '', 'zzrtzy', 'tzyer', '#000080', '2t98258', 15000, '2019-07-04', 4500, 0),
+(18, '', 'foot', '365959', '#008040', '6549+595', 49595, '2019-07-11', 6150, 0),
+(19, '', 'uiheio', '59595626', '#00ff00', 'df665fg', 120000, '2019-07-10', 2560, 0),
+(21, '', 'cheval', '98484626', '#008040', 'fg576sh', 12489, '2019-07-04', 48352, 0),
+(23, '', 'kawasaki', '1871285', '#000080', 'hd-432-kr', 14620, '2019-08-15', 14000, 0),
+(29, '', 'shop', '41911174842', '#ff8000', 'sq892kj', 15054, '2019-08-07', 2000, 0);
 
 --
 -- Index pour les tables déchargées
@@ -240,7 +257,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `id_vehicle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_vehicle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
