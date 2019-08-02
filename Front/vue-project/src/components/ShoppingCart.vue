463c3ec4e2f20611315c78cfc57bc088511d610e
Demba <m.demba.dia@gmail.com>
2019-08-02 16:21:52 +0200

    Create ShoppingCart.vue

---
+++ Front/vue-project/src/components/ShoppingCart.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container fluid"> 
+        <h1>Shopping Cart</h1>
+        
+        <ul>
+            <li v-for="vehicle in vehicles" :key="vehicle.id">
+                {{ vehicle.title }} - {{ vehicle.price}} - {{ vehicle.available }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        vehicles() {
+           
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
