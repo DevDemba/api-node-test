c44a01a45a0dea47d1a8f7ec899421b361f0ee01
DemsDevops <m.demba.dia@gmail.com>
2019-08-17 22:19:19 +0200

    Create Fidelity.vue

---
+++ Front/vue-project/src/components/Fidelity.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container fluid">
+        <h1>Fidelity</h1>
+        <h4>Best customers</h4>
+
+        <div class="container card">
+            
+            <table class="cart_table">
+                <tr class="table title">
+                    <th v-for="column in columns" :key="column.index">{{column}}</th>
+                    <th> </th>
+                </tr>
+                <tr class="table_value">
+                    <td>1</td>
+                    <td>test</td>
+                    <td>15</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'fidelity',
+    data(){
+        return {
+            columns: ['id', 'user', 'point']
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+table th, td{
+    text-align: center;
+}
+</style>
+
