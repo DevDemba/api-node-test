20d01e5c0455598c729005181189c8f4f285d6d1
DemsDevops <m.demba.dia@gmail.com>
2019-08-21 20:31:38 +0200

    require bcrypt js

--- Backend/Router/UserRouter.js
+++ Backend/Router/UserRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const dbConn = require('../database/db').dbConn;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 // getting the local authentication type
 const passport = require('passport');
